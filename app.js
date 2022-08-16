bd9522556cb2627119a41da6f5fe94954b119c40
SofiaGieco <sofiagieco@gmail.com>
2022-08-15 23:22:55 -0300

    First commit

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require("express")
+
+const dotenv = require("dotenv").config()
+
+const app = express()
+
+const path = require("path")
+
+app.use(express.static("public"))
+
+app.listen(process.env.PORT, function(){
+    console.log("Servidor funcionando en el puerto " + process.env.PORT)
+})
+
+app.get("/", function(req,res){
+    res.sendFile(path.join(__dirname, "views/home.html"))
+})
+
+app.get("/index.html", function(req,res){
+    res.sendFile(path.join(__dirname, "views/index.html"))
+})
+
+app.get("/login.html", function(req,res){
+    res.sendFile(path.join(__dirname, "views/login.html"))
+})
+
+app.post("/index.html", function(req,res){
+    res.sendFile(path.join(__dirname, "views/home.html"))
+})
+
+app.post("/login.html", function(req,res){
+    res.sendFile(path.join(__dirname, "views/home.html"))
+})
+
